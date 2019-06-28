e90178be6d3a17c1e1d82259afd7329e557b7d45
lawtech0902 <584563542@qq.com>
2019-06-28 14:59:05 +0800

    add icons

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -24,14 +24,17 @@ export default {
   .header
     display: flex
     line-height: .86rem
-    background: $bgcolor
+    background: $bgColor
     color: #fff
+
     .header-left
       width: .64rem
       float: left
+
       .back-icon
         text-align: center
         font-size: .4rem
+
     .header-input
       flex: 1
       height: .64rem
@@ -42,10 +45,12 @@ export default {
       background: #fff
       border-radius: .1rem
       color: #ccc
+
     .header-right
       width: 1.24rem
       float: right
       text-align: center
+
       .arrow-icon
         margin-left: -0.04rem
         font-size: .24rem
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -24,7 +24,7 @@ export default {
       }, {
         id: '0002',
         imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/87a224d0349d94a11e97f31aa1aba4f5.jpg_750x200_1f78af87.jpg'
-      } ]
+      }]
     }
   }
 }
@@ -33,12 +33,14 @@ export default {
 <style lang="stylus" scoped>
   .wrapper >>> .swiper-pagination-bullet-active
     background-color: #fff !important
+
   .wrapper
     overflow: hidden
     background-color: #ddd
     width: 100%
     height: 0
     padding-bottom: 26.7%
+
     .swiper-img
       width: 100%
 </style>
